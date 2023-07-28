b18a06342248fc9d2e36f90a5e0aa5a278142a3c
ThiagoThomas09 <thiagothomas09@outlook.com>
2023-07-28 15:10:04 -0300

    Extrai urls hardcoded

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -2,17 +2,21 @@ from django.conf import settings
 from django.contrib import messages
 from django.core import mail
 from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url as r
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-def subscribe(request): #delega para o create ou para o new
+def new(request): #delega para o create ou para o new
     if request.method == 'POST':
         return create(request)
-    else:
-        return new(request)
+        
+    return empty_form(request)
+
+def empty_form(request):
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscriptions/subscription_form.html', context)
 
 
 def create(request):
@@ -31,11 +35,7 @@ def create(request):
                 {'subscription': subscription})
 
 
-    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
-
-def new(request):
-    context = {'form': SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
 
 def detail(request, pk):
     try:
